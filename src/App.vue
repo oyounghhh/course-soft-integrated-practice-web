04d45c59a2d6fc97aea09a1937e93d09b8347da1
linhieng <790728740@qq.com>
2023-10-10 13:40:02 +0800

    fix: 修复 el-table 的 ResizeObserver 问题

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
     <router-view />
 </template>
+
+<script>
+// ------------------ 解决 ResizeObserver loop completed with undelivered notifications. ------------------
+const debounce = (fn, delay) => {
+    let timer = null
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
+const _ResizeObserver = window.ResizeObserver
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+    constructor(callback) {
+        callback = debounce(callback, 16)
+        super(callback)
+    }
+}
+</script>
