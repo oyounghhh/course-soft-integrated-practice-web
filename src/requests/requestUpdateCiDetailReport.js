eff045a00211c24c5517ae775d4cceaf0a00cc65
linhieng <790728740@qq.com>
2023-10-11 10:48:11 +0800

    refactor: 接口-更新 ciDetailedReport 数组

--- src/requests/requestUpdateCiDetailReport.js
+++ src/requests/requestUpdateCiDetailReport.js
@@ -2,8 +2,8 @@ import { post } from '@/requests'
 
 /**
  * 更新具体指标项的值
- * @param {*} data
- * @returns
+ * @param {Array<CiDetailedReport>} data
+ * @returns {number} 1 表示成功
  */
 export default function requestUpdateCiDetailReport(data) {
     return post('/ciDetailedReport/updateCiDetailedReport', data)
