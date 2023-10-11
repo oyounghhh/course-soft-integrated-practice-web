b2d1d7a04d259b6f2438957333559f02a5f59947
linhieng <790728740@qq.com>
2023-10-11 10:05:20 +0800

    refactor: 接口-创建体检报告模版

--- src/requests/requestReportTemplate.js
+++ src/requests/requestReportTemplate.js
@@ -2,8 +2,8 @@ import { post } from '@/requests'
 
 /**
  *
- * @param {{orderId: string, smId: string}} data
- * @returns
+ * @param {Orders} data
+ * @returns {number} 1 表示创建成功
  */
 export default function requestReportTemplate(data) {
     return post('/ciReport/createReportTemplate', data)
