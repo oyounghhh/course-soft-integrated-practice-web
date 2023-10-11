ff6b74f9a23757ca8f9571c99057dd6b49c07dc9
linhieng <790728740@qq.com>
2023-10-11 10:41:40 +0800

    refactor: 接口-归档

--- src/requests/requestUpdateCiReportResult.js
+++ src/requests/requestUpdateCiReportResult.js
@@ -2,8 +2,8 @@ import { post } from '@/requests'
 
 /**
  * 更新多项指标项的值，也就是将总检结论归档
- * @param {*} data
- * @returns
+ * @param {Orders} data
+ * @returns {number} 1 表示归档成功
  */
 export default function requestUpdateCiReportResult(data) {
     return post('/orders/updateOrdersState', data)
